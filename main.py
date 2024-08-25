906c6b3c5f69216bfa3a365c00c9e75a202ad295
Md Tabish <tabishcoding0101@gmail.com>
2024-08-25 15:00:17 +0530

    Changes

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 from firebase import get_image_url
 import json
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@cross_origin()
 
 @app.route('/upload', methods=['POST'])
 def upload_csv():
@@ -15,17 +20,12 @@ def upload_csv():
         return jsonify({'error': 'File not proper'}), 400
 
 
-@app.route('/get-data', methods=['GET'])
+@app.route('/get-data', methods=['GET', 'POST'])
 def get_image_data():
     # Example data; in a real scenario, this might come from a database or other source
     f=open('data.json')
-    image_data = json.load(f)
-
-    # Add the image URLs to the data
-    for item in image_data:
-        item['image_url'] = get_image_url(item['ecg/image_name'])
-
-    return jsonify(image_data)
+    data = json.load(f)
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
